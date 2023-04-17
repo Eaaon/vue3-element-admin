3de7d921060dba3ea0d969eb6175480afaa69c24
Eaaon <614543415@qq.com>
2023-04-17 23:08:06 +0800

    project

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
+const resolve = (dir) => path.join(__dirname, '.', dir);
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.resolve.alias
+    .set('@', resolve('src')) 
+    .set('api', resolve('src/apis'))
+  }
+})
